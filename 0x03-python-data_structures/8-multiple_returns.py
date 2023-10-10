32ef906d2894b56f1c302235f0d78be93520a953
Solomon Bassey <Solomonbassey20@gmail.com>
2023-10-10 03:43:44 +0100

    task 8 fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def multiple_returns(sentence): 
-     str_len = len(sentence) 
-     first_char = None if str_len == 0 else sentence[0] 
-     return (str_len, first_char)
+def multiple_returns(sentence):
+    str_len = len(sentence)
+    first_char = None if str_len == 0 else sentence[0]
+    return (str_len, first_char)
