849151a23772cecc776d4ea7db138ace16f30a9a
Solomon Bassey <solomonbassey20@gmail.com>
2023-10-12 02:35:47 +0000

    Update

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):7
+    if my_list == []:
+        return 0
+    add_score = 0
+    add_weight = 0
+    for _tuple in my_list:
+        add_score += _tuple[0] * _tuple[1]
+        add_weight += _tuple[1]
+    return add_score / add_weight
