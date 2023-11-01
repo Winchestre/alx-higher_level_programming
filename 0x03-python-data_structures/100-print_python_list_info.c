01d46ab6142e42e0ad8932f3b005b9979db017ec
Solomon Bassey <Solomonbassey20@gmail.com>
2023-11-01 06:46:50 +0100

    forced hand

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,14 +1,15 @@
 #include <Python.h>
+#include </usr/include/python2.7/pyconfig-64.h>
 #include <stdio.h>
 
 void print_python_list_info(PyObject *p)
 {
-	int size, alloc, i;
+	int size, is_alloc, i;
 
 	size = Py_SIZE(p);
-	alloc = ((PyListObject *) p)->alloc;
+	is_alloc = ((PyListObject *) p)->is_alloc;
 	printf("[*] Size of the Python List = %d\n", size);
-	printf("[*] Allocated = %d\n", alloc);
+	printf("[*] Allocated = %d\n", is_alloc);
 
 	for (i = 0; i < size; i++)
 		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
