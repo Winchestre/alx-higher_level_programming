186e04a34f29631d47aa4427fae1744b8ab9e8a9
Solomon Bassey <Solomonbassey20@gmail.com>
2023-11-03 15:27:15 +0100

    first refactor

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,36 +1,23 @@
 #!/usr/bin/python3
 """
-<<<<<<< HEAD
 Module text_indentation
 Adds two new lines after a set of characters.
-=======
-    Module text_indentation
-    Adds two new lines after a set of characters.
->>>>>>> a707dffc2cd8e9554d9d7ed7451b99286e0733f9
 """
 
 
 def text_indentation(text):
-<<<<<<< HEAD
     """Prints text with added two newlines
     after each of these characters {'.', '?', ':'}.
-
-=======
-    """
-        Prints text with added two newline
-        after each of these characters {'.', '?', ':'}.
->>>>>>> a707dffc2cd8e9554d9d7ed7451b99286e0733f9
     """
 
     if type(text) is not str:
         raise TypeError("text must be a string")
     for delim in ".:?":
-<<<<<<< HEAD
-        text = (delim + "\n\n").join([line.strip(" ")
-            for line in text.split(delim)])
-=======
         text = (delim + "\n\n").join(
             [line.strip(" ") for line in text.split(delim)])
->>>>>>> a707dffc2cd8e9554d9d7ed7451b99286e0733f9
 
-        print("{}".format(text), end="")
+    print("{}".format(text), end="")
+    
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
