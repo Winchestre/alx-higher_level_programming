d03f661477a5a79d37b7ff6106439089aca5e938
Solomon Bassey <Solomonbassey20@gmail.com>
2024-02-22 12:27:02 +0100

    my apologies :(

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@
 // number matches a given integer.
 
 const request = require('request');
-const url = `https://swapi-api.alx-tools.com/api/films/{process.argv[2]}`;
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
 request.get(url, (error, response, body) => {
   if (error) {
